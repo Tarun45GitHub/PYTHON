521b7ecc7ee8268459bd751083c0e903f9d0bad1
TARUN DALBERA <dalberatarun1@gmail.con>
2024-10-09 16:16:53 +0530

    number

---
+++ 01DataType/Number.py
@@ -0,0 +1,53 @@
+print("Number")
+x=2
+y=3
+z=4
+print(3**2) #3^2=9
+print('tarun'+'dalber') #operator overloading
+
+print(x,y,z)
+
+print(1/3)
+
+print(repr(123)) #Returns an unambiguous string representation of the object
+
+print(str(123)) #Returns a human-readable string representation of the object
+
+print(x<y<z) # x<y and y<z 
+
+import math
+print(math.floor(4.5))  # lowest rounded value
+print(math.floor(-3.77)) # lowest rounde value
+print(math.trunc(-2.8)) # take towerds Zero
+
+print(0b100) # binary
+print(0o20)  # octal
+print(0xFF)  # hexal
+print(oct(56))
+print(hex(255))
+print(bin(20))
+
+
+print(int(3.2))
+print(int('111',2))
+
+print(5<<3)
+
+import random
+print(random.randint(1,10))
+
+print((0.1+0.1+0.1)-0.3)
+
+print({1,2,3,4}&{1,2,3}) # intersection
+print({1,2,3,4}|{1,5,6}) # union
+print({1,2,3,4}-{1,2,3}) # minus
+print({1,2,3}-{1,2,3}) # set() not {}
+print(type({}))  # dictionary
+
+
+
+
+
+
+
+
--- 01DataType/datatypes.md
+++ 01DataType/datatypes.md
@@ -1,12 +1,22 @@
 # Data Types /Object Types
 
-- Number :1234,3.14, 3i+4j(complex number),0b11,Decinal(),Fraction()
-- string : 'spam',"string",b'a\x01c',u'sp\xc4m',"bob's"
-- List :[1,[2,3,'four'],5.6],list(range(10))
-- Tuple : (1,'spam',4,'U'),tuple('spam'),namedtuple
-- Dict(Dictionary) : {'key':'value'} ,dict(hours=10)
-- set : set('abc'),{'a','b','c'}
-- file : open(file path)
-- boolean: true or false
-- None: not value
+## Number 
+- 1234, 3.14, 3i+4j(complex number), 0b11, Decimal(),Fraction()
+## string 
+- 'spam',"string",b'a\x01c',u'sp\xc4m',"bob's"
+## List 
+- [1,[2,3,'four'],5.6],list(range(10))
+## Tuple 
+- (1,'spam',4,'U'),tuple('spam'),namedtuple
+## Dict(Dictionary) 
+- {'key':'value'} ,dict(hours=10)
+## set 
+ set('abc'),{'a','b','c'}
+## file  
+- open(file path)
+## boolean 
+- true or false
+# None
+- not value
+
 - functions,modules,class
